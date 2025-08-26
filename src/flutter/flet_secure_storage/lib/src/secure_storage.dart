cadcb7b3c1196b2055a3caa04ad3ab4ca159dc91
td3447 <59424730+td3447@users.noreply.github.com>
2025-08-26 19:28:42 -0400

    Renamed and updated
    Changed name from flet_secure_storage to secure_storage to best match the current naming conventions.
    
    Covered all the api calls that flutter_secure_storage has, and mapped them to python calls. Each call returns the same type that is expected of the client_storage calls from SharedPreferences

---
+++ src/flutter/flet_secure_storage/lib/src/secure_storage.dart
@@ -0,0 +1,75 @@
+import 'package:flet/flet.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SecureStorageService extends FletService {
+  SecureStorageService({required super.control});
+
+  // Initialize _storage from FlutterSecureStorage later
+  late final FlutterSecureStorage _storage;
+
+  @override
+  void init() {
+    super.init(); // Calls FletService.init()
+
+    _storage = const FlutterSecureStorage(
+        aOptions: AndroidOptions(
+            // TODO: This will be removed in the next version and will default to true.
+            // https://github.com/juliansteenbakker/flutter_secure_storage?tab=readme-ov-file#important-notice-for-android
+            encryptedSharedPreferences: true,
+            sharedPreferencesName: "FletSecureStorage"),
+        iOptions: IOSOptions(),
+        mOptions: MacOsOptions(),
+        lOptions: LinuxOptions(),
+        wOptions: WindowsOptions(),
+        webOptions: WebOptions(
+            dbName: "FletSecureStorage", publicKey: "FletSecureStorage"));
+
+    debugPrint(
+        "SecureStorageService(${control.id}).init: ${control.properties}");
+    control.addInvokeMethodListener(_invokeMethod);
+  }
+
+  @override
+  void update() {
+    debugPrint(
+        "SecureStorageService(${control.id}).update: ${control.properties}");
+  }
+
+  Future<dynamic> _invokeMethod(String name, dynamic args) async {
+    switch (name) {
+      case "set": // SharedPreferences / client_storage returns bool
+        await _storage.write(key: args["key"]!, value: args["value"]!);
+        return true;
+
+      case "get":
+        return _storage.read(key: args["key"]!);
+
+      case "contains_key": // Returns bool
+        return _storage.containsKey(key: args["key"]!);
+
+      // TODO: get all keys with the same prefix
+      // case "get_keys":
+      //   return _storage.containsKey().where((key) => key.startsWith(args["key_prefix"]!)).toList();
+
+      case "remove":
+        await _storage.delete(key: args["key"]!);
+        return true;
+
+      case "clear": // SharedPreferences / client_storage returns bool
+        await _storage.deleteAll();
+        return true;
+
+      default:
+        throw Exception("Unknown SecureStorage method: $name");
+    }
+  }
+
+  @override
+  void dispose() {
+    debugPrint("SecureStorageService(${control.id}).dispose()");
+    control.removeInvokeMethodListener(_invokeMethod);
+    super.dispose();
+  }
+}
