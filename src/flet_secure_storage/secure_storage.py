b48874ddac566e152df8e54b052fdb90035be82f
td3447 <59424730+td3447@users.noreply.github.com>
2025-08-27 23:39:02 -0400

    Changed imports, added functionality
    * Added get_keys function. To keep it similar to shared_preferences, I added a string input to check the returned keys against the input.
    * Added comments explaining the return and function of get_keys
    * Removed the individual imports of control and service
    * Added flet as ft import. To ensure functionality.

--- src/flet_secure_storage/secure_storage.py
+++ src/flet_secure_storage/secure_storage.py
@@ -1,13 +1,12 @@
 from typing import Any, Optional
 
-from flet.controls.base_control import control
-from flet.controls.services.service import Service
+import flet as ft
 
 __all__ = ["SecureStorage"]
 
 
-@control("SecureStorage")
-class SecureStorage(Service):
+@ft.control("SecureStorage")
+class SecureStorage(ft.Service):
     """
     Create an instance of FlutterSecureStorage in Flet
     https://pub.dev/packages/flutter_secure_storage
@@ -71,17 +70,27 @@ class SecureStorage(Service):
         """
         return await self._invoke_method("remove", {"key": key})
 
-    # TODO: shared_preferences -> storage.getKeys
-    # async def get_keys(self, key_prefix: str) -> list[str]:
-    #     """_summary_
+    async def get_keys(self, key_prefix: Optional[str]) -> list[str]:
+        """
+        Retrieves all keys from secure storage.
+        From flutter_secure_storage: storage.readAll
 
-    #     Args:
-    #         key_prefix (str): _description_
+        Args:
+            key_prefix (str): The prefix to filter keys by.
 
-    #     Returns:
-    #         list[str]: _description_
-    #     """
-    #     return await self._invoke_method("get_keys", {"key_prefix": key_prefix})
+        Returns:
+            list[str]: Returns a list of keys [<key_prefix>:<value>] of the values
+                       that match the key_prefix or all keys if the user enters and
+                       empty string or None
+        """
+        key_prefix = key_prefix or ""
+        response: dict[str, str] = await self._invoke_method("get_keys")
+
+        return [
+            f"{key}:{value}"
+            for key, value in response.items()
+            if key.startswith(key_prefix)
+        ]
 
     async def clear(self) -> bool:
         """
