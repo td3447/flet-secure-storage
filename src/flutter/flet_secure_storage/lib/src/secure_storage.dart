5ebed35f79f17f1d8a206182e4dfa94a92924cd4
td3447 <59424730+td3447@users.noreply.github.com>
2025-08-27 23:35:16 -0400

    Updated comments, package import and added feature
    * Added readAll() to get all keys from storage
    * Added comments listing the return types of the functions
    * Added deprecated "tag" to encryptedSharedPreferences to annotate it will be removed in the next full version. Version 10
    * Added flutter/foundation.dart to keep debugPrint functionality.
    * Removed unnessecary cupertino and widgets packages

--- src/flutter/flet_secure_storage/lib/src/secure_storage.dart
+++ src/flutter/flet_secure_storage/lib/src/secure_storage.dart
@@ -1,6 +1,5 @@
 import 'package:flet/flet.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class SecureStorageService extends FletService {
@@ -15,7 +14,7 @@ class SecureStorageService extends FletService {
 
     _storage = const FlutterSecureStorage(
         aOptions: AndroidOptions(
-            // TODO: This will be removed in the next version and will default to true.
+            // TODO: Deprecated- encryptedSharedPreferences will be removed in the next version (10) and will default to true.
             // https://github.com/juliansteenbakker/flutter_secure_storage?tab=readme-ov-file#important-notice-for-android
             encryptedSharedPreferences: true,
             sharedPreferencesName: "FletSecureStorage"),
@@ -39,25 +38,24 @@ class SecureStorageService extends FletService {
 
   Future<dynamic> _invokeMethod(String name, dynamic args) async {
     switch (name) {
-      case "set": // SharedPreferences / client_storage returns bool
+      case "set": // Returns bool
         await _storage.write(key: args["key"]!, value: args["value"]!);
         return true;
 
-      case "get":
+      case "get": // Returns String?
         return _storage.read(key: args["key"]!);
 
       case "contains_key": // Returns bool
         return _storage.containsKey(key: args["key"]!);
 
-      // TODO: get all keys with the same prefix
-      // case "get_keys":
-      //   return _storage.containsKey().where((key) => key.startsWith(args["key_prefix"]!)).toList();
+      case "get_keys": // returns Map<String, String>
+        return _storage.readAll();
 
-      case "remove":
+      case "remove": // Returns bool
         await _storage.delete(key: args["key"]!);
         return true;
 
-      case "clear": // SharedPreferences / client_storage returns bool
+      case "clear": // Returns bool
         await _storage.deleteAll();
         return true;
 
